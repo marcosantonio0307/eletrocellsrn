c1b6bcdf988a862df38a0216730c26340b328987
Marcos Antonio <marcosantoniotaina@gmail.com>
2019-07-26 10:38:59 -0300

    change in add item sale and modification in report expenses day

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,4 +3,9 @@ class Sale < ApplicationRecord
   belongs_to :user
 
   has_many :item, dependent: :destroy
+
+  def update_total
+  	new_total = item.map{|i| i.price}.sum
+  	update(total: new_total)
+  end
 end
