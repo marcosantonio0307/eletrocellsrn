095772ab599ef15735346649e6e8ccb8f6627b85
Marcos Antonio <marcosantoniotaina@gmail.com>
2020-01-10 23:20:22 -0300

    add imei and warranty to sales

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_152204) do
+ActiveRecord::Schema.define(version: 2020_01_11_021015) do
 
   create_table "cashes", force: :cascade do |t|
     t.decimal "total"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 2019_06_10_152204) do
     t.string "category"
     t.string "description"
     t.string "status"
+    t.string "imei"
+    t.string "warrenty"
     t.index ["client_id"], name: "index_sales_on_client_id"
     t.index ["user_id"], name: "index_sales_on_user_id"
   end
